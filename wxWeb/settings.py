16350fed8cfdc3db585281ca05f2680c88226024
zhaoun <1159001728@qq.com>
2020-02-02 21:22:31 +0800

    微信小程序

--- wxWeb/settings.py
+++ wxWeb/settings.py
@@ -113,9 +113,9 @@ REST_FRAMEWORK = {
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
